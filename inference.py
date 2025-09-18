978d90211d0d87c96085cee0f866ff83b2c39131
Esben Skov <ehs@sa.dk>
2025-09-18 10:19:42 +0200

    added time

--- inference.py
+++ inference.py
@@ -2,7 +2,7 @@ from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, Auto
 from qwen_vl_utils import process_vision_info
 import torch
 import os
-
+import time
 # Hard-disable FlashAttention2 so Transformers won't try to import it
 os.environ["TRANSFORMERS_ATTENTION_IMPLEMENTATION"] = "sdpa"
 
@@ -49,9 +49,13 @@ inputs = processor(
     return_tensors="pt",
 )
 inputs = inputs.to("cuda")
+start_time = time.time()
 
 # Inference: Generation of the output
+print("Start to generate...")
 generated_ids = model.generate(**inputs, max_new_tokens=128)
 generated_ids_trimmed = [out_ids[len(in_ids):] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)]
 output_text = processor.batch_decode(generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False)
 print(output_text)
+end_time = time.time()
+print(f"Time used: {end_time - start_time} seconds")
