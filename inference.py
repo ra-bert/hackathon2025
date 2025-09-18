9999e71c347d97f34fd1b02aaad5a32a4f50018d
Esben Skov <ehs@sa.dk>
2025-09-18 10:34:13 +0200

    addin new test dataset

--- inference.py
+++ inference.py
@@ -3,6 +3,9 @@ from qwen_vl_utils import process_vision_info
 import torch
 import os
 import time
+import pandas as pd
+
+dataset = pd.read_csv('norhand/test/textlines_df.csv')
 # Hard-disable FlashAttention2 so Transformers won't try to import it
 os.environ["TRANSFORMERS_ATTENTION_IMPLEMENTATION"] = "sdpa"
 
